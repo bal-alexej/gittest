6d60833a45433114dc5187375f514a27bcc60fba
Alexey Balyasnikov <mr.uzumaki6@yandex.ru>
2022-02-20 15:04:40 +0300

    загрузка

---
+++ dist/dist/js/script.js
@@ -0,0 +1,7 @@
+// function range() {
+//   var val = $(".range").val();
+//   $(".range").css({
+//     background:
+//       "-webkit-linear-gradient(left, #67ac5b 0%, #67ac5b '+val+'%, #fff '+val+'%, #fff 100% )",
+//   });
+// }
