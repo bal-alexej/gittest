ae10a7f5cf6a6a0d540c218ee0f3f08f795283b1
Alexey Balyasnikov <mr.uzumaki6@yandex.ru>
2022-02-20 16:09:21 +0300

    загрузка изменений для мобильной версии

--- dist/dist/js/script.js
+++ dist/dist/js/script.js
@@ -5,3 +5,14 @@
 //       "-webkit-linear-gradient(left, #67ac5b 0%, #67ac5b '+val+'%, #fff '+val+'%, #fff 100% )",
 //   });
 // }
+var connectSlider = document.getElementById("slider-connect");
+
+noUiSlider.create(connectSlider, {
+  start: 40,
+  connect: "lower",
+  step: 1,
+  range: {
+    min: 0,
+    max: 100,
+  },
+});
